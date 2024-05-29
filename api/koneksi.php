322638ca87038e4b646c835d40544fe36b34fb15
Muhammad Dwiky Alfira <dwikyalfira1@gmail.com>
2024-05-29 21:03:02 +0700

    update

--- api/koneksi.php
+++ api/koneksi.php
@@ -4,7 +4,7 @@ $koneksi = mysqli_connect("localhost", "root", "", "project_ecommerce");
 
 if($koneksi){
 
-	// echo "Database berhasil konek";
+	// echo "Database berhasil terkoneksi";
 	
 } else {
 	echo "gagal Connect";
